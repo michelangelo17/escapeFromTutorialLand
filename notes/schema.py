d05889557d3469921a08aac65c8a2b824606df93
michelangelo17 <michelangelo.markus@gmail.com>
2020-05-29 18:41:40 -0700

    added get by id

--- notes/schema.py
+++ notes/schema.py
@@ -12,9 +12,22 @@ class NoteType(DjangoObjectType):
 
 class Query(ObjectType):
     all_notes = graphene.List(NoteType)
+    note = graphene.Field(NoteType, id=graphene.Int(), title=graphene.String())
 
     def resolve_all_notes(self, info, **kwargs):
         return Note.objects.all()
+
+    def resolve_note(self, info, **kwargs):
+        id = kwargs.get('id')
+        title = kwargs.get('title')
+
+        if id is not None:
+            return Note.objects.get(pk=id)
+
+        if title is not None:
+            return Note.objects.get(title=title)
+
+        return None
 
 
 schema = graphene.Schema(query=Query)
